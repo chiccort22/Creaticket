4588c2ac673ed72be100166c3bf419c4fde0148a
chiccort22 <chico.rt22@gmail.com>
2026-09-20 23:57:03 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = 'creaticket-pwa-v1';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(r => { const copy=r.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return r; }).catch(()=>caches.match(event.request).then(r=>r || caches.match('./index.html'))));
+});
